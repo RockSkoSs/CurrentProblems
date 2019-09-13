f4f714f63f4ac8181f56634d8a7999b39ab3ba0d
RockSkoSs <55146030+RockSkoSs@users.noreply.github.com>
2019-09-13 18:18:53 +0200

    Add files via upload

---
+++ kurwa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool Done = false;
+        string imie;
+
+        while (Done == false)
+        {
+            Console.WriteLine("Podaj kobiete");
+            imie = Console.WriteLine();
+
+            if (imie[imie.Length - 1] == 'a') ;
+            {
+                Done = true;
+                Console.WriteLine("czesc +name");
+
+            }
+            else
+            {
+                Console.WriteLine("dales meskie");
+            }
+        }
+        Console.Read();
+    }
+}
